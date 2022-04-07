19474d944b2cb2c328d7c1ebfc6db82913ed52cb
Travis <arma3cloud@gmail.com>
2022-04-07 16:24:09 -0700

    Update Team.jsx

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -6,7 +6,7 @@ const Team = () => {
   const ourTeam = [
     { name: "Connor", role: "Studio Head & Art Director", color: "#1c1f20", avatar: "images/avatars/connor.webp", socialLinks: { youtube: "", github: "", artstation: "https://www.artstation.com/connorb3d" } },
     { name: "Ryan", role: "Project Manager", color: "#1c1f20", avatar: "images/avatars/ryan.webp", socialLinks: { youtube: "", github: "", artstation: "https://www.artstation.com/ryanlevac" } },
-    { name: "Cameron", role: "Lead Programmer", color: "#1c1f20", avatar: "images/avatars/cameron.webp", socialLinks: { youtube: "https://www.youtube.com/channel/UC3DPVEG4Nhgeylw6EhAasSQ", github: "", artstation: "" } },
+    { name: "Cameron", role: "Lead Programmer", color: "#1c1f20", avatar: "images/avatars/cameron.webp", socialLinks: { youtube: "https://www.youtube.com/channel/UC3DPVEG4Nhgeylw6EhAasSQ", github: "https://github.com/IRSMsoso", artstation: "" } },
     { name: "Tengo", role: "Programmer & Animator", color: "#1c1f20", avatar: "images/avatars/tengo.webp", socialLinks: { youtube: "https://www.youtube.com/channel/UCmx4gGmsSBYA_G4QqVeXUng", github: "https://github.com/Tengoz", artstation: "https://www.artstation.com/artwork/X1glWn" } },
     { name: "Travis", role: "Website Developer", color: "#1c1f20", avatar: "images/avatars/billyjoe.jpg", socialLinks: { youtube: "", github: "https://github.com/TravisDevLife", artstation: "" } },
     { name: "Robin", role: "Level Designer & Animator", color: "#1c1f20", avatar: "images/avatars/runkle.webp", socialLinks: { youtube: "https://www.youtube.com/channel/UCbzIQfF8efrF9YMsxuNzpEw", github: "", artstation: "https://www.artstation.com/runkletookle" } },
